5be763a2312ce52c5e1a4d6873ccf7c8ed086d90
Oskar <>
2023-01-13 17:51:50 +0100

    create first main menu

--- src/main.py
+++ src/main.py
@@ -2,4 +2,7 @@ from settings import Colors
 
 print(f"{Colors.YELLOW}====================================={Colors.END}")
 print(f"{Colors.YELLOW}======= Welcome in Mathmatica ======={Colors.END}")
-print(f"{Colors.YELLOW}====================================={Colors.END}")
+print(f"{Colors.YELLOW}====================================={Colors.END}\n")
+
+print(f"{Colors.LIGHT_BLUE}What do you want to calculate?{Colors.END}")
+print(f"{Colors.BLUE}1. The transpose of matrix{Colors.END}")
